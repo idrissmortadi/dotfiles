67a602aba2d677e5ffe78eb0b7110b58a2ad8d7f
Idriss Mo <idrissmortadi@gmail.com>
2025-04-30 11:02:59 +0200

    Group themes together

---
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,11 @@
+return {
+  { "Mofiqul/adwaita.nvim" },
+  { "catppuccin/nvim" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
